bbab66f485133f436c342ec737370fa99fb9c996
Paymon Teyer <pteyer@gmail.com>
2014-08-11 22:57:30 -0700

    adding more messages

--- src/main/java/com/pteyer/jrz/Constants.java
+++ src/main/java/com/pteyer/jrz/Constants.java
@@ -4,6 +4,7 @@ public final class Constants {
     public static class Main {
         public static final String USAGE_STRING = "Usage: ...";
         public static final String EXCEPTION_MESSAGE_CONFIG_VALUE_MISSING = "value for config argument is missing";
+        public static final String EXCEPTION_MESSAGE_CONFIG_INVALID_FILE = "invalid configuration file";
         public static final String EXCEPTION_MESSAGE_PREFIX_INVALID_ARGUMENT = "invalid argument: ";
     }
 }
--- src/main/java/com/pteyer/jrz/Main.java
+++ src/main/java/com/pteyer/jrz/Main.java
@@ -1,11 +1,14 @@
 package com.pteyer.jrz;
 
+import com.google.common.io.Files;
 import org.eclipse.jetty.server.Server;
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.misc.IOUtils;
 
+import java.io.File;
 import java.io.PrintStream;
 import java.net.URI;
 import java.util.Arrays;
@@ -21,7 +24,7 @@ public class Main {
         printStream.print(USAGE_STRING);
     }
 
-    public static void main(final String[] cliArguments) throws Exception {
+    public static void main(final String[] cliArguments) {
         if (cliArguments.length >= 1) {
             final String firstArgument = cliArguments[0];
             if (("help".equalsIgnoreCase(firstArgument)
@@ -34,10 +37,18 @@ public class Main {
                     || ("--config".equalsIgnoreCase(firstArgument))
                     || ("-c".equalsIgnoreCase(firstArgument))) {
                 if (cliArguments.length < 2) {
+                    logger.error("configuration file is not passed -> showing help and then exit");
                     showUsage(System.err);
                     throw new IllegalArgumentException(EXCEPTION_MESSAGE_CONFIG_VALUE_MISSING);
                 }
                 final String secondArgument = cliArguments[1];
+                final File configFile = new File(secondArgument);
+                if (!configFile.isFile() || !configFile.canRead()) {
+                    logger.error("configuration file is not readable -> showing help and then exit");
+                    showUsage(System.err);
+                    throw new IllegalArgumentException(EXCEPTION_MESSAGE_CONFIG_INVALID_FILE);
+                }
+
             } else {
                 // all parameters has to be passed as environment variables
                 showUsage(System.err);
@@ -51,8 +62,8 @@ public class Main {
         final ResourceConfig config = new ResourceConfig()
                 .packages("com.pteyer.jrz");
         Server server = JettyHttpContainerFactory.createServer(URI.create(BASE_URI), config);
-        server.start();
-        server.join();
+//        server.start();
+//        server.join();
     }
 
     private void loadConfigurations() {
